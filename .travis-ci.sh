feeb7efb884078008f8ecc7331fdc3a47980c0dd
Tony Cosentini <tonyc@uber.com>
2015-12-10 17:21:55 -0800

    One more try.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,9 @@
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-opam.sh
+bash -ex .travis-opam.sh
+
+export OPAMYES=1
+eval `opam config env`
+
 opam init --no-setup
 opam update
 opam pin add --yes --no-action .
